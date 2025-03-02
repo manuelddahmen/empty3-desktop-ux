7ce60bad37751a77ad3a231130b6aa1f462d015e
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2025-03-02 16:25:45 +0100

    Google Cloud Function
    Signed-off-by: Manuel Daniel Dahmen <manuel.dahmen@gmx.com>

--- src/main/java/one/empty3/apps/facedetect/gcp/ImageProcessor.java
+++ src/main/java/one/empty3/apps/facedetect/gcp/ImageProcessor.java
@@ -19,12 +19,11 @@ public class ImageProcessor implements HttpFunction {
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
-        try {
-
-            // Set CORS headers to allow requests from Flutter
-            response.appendHeader("Access-Control-Allow-Origin", "*"); // Replace with your Flutter app's origin in production
-            response.appendHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-            response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+//        try {
+        // Set CORS headers to allow requests from Flutter
+        response.appendHeader("Access-Control-Allow-Origin", "*"); // Replace with your Flutter app's origin in production
+        response.appendHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+        response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
 
         if ("OPTIONS".equals(request.getMethod())) {
             // Respond to preflight requests
@@ -37,92 +36,91 @@ public class ImageProcessor implements HttpFunction {
             return;
         }
 
-            // Read the request body
-            BufferedReader reader = request.getReader();
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-            String requestBody = stringBuilder.toString();
+        // Read the request body
+        BufferedReader reader = request.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String requestBody = stringBuilder.toString();
 
-            Logger.getLogger("Cloud function ImageProcessor");
-            Logger.getLogger(this.getClass().getCanonicalName())
-                    .log(Level.INFO, requestBody);
+        Logger.getLogger("Cloud function ImageProcessor");
+        Logger.getLogger(this.getClass().getCanonicalName())
+                .log(Level.INFO, requestBody);
 
-            // Parse the JSON request body
-            Gson gson = new Gson();
-            Map<String, String> data = new HashMap<>();
-            JsonObject jsonObject = gson.fromJson(request.getReader(), JsonObject.class);
-            if(jsonObject==null) {
-                response.setContentType("application/json");
-                response.setStatusCode(200);
-                response.getWriter().write("jSonObject is null in ImageProcessor"+"\n");
-                return;
-            }
-
-            if (jsonObject.has("image1")) {
-                data.put("image1", jsonObject.get("image1").getAsString());
-            }
-            if (jsonObject.has("model")) {
-                data.put("model", jsonObject.get("model").getAsString());
-            }
-            if (jsonObject.has("image3")) {
-                data.put("image3", jsonObject.get("image3").getAsString());
-            }
-            if (jsonObject.has("textFile1")) {
-                data.put("textFile1", jsonObject.get("textFile1").getAsString());
-            }
-            if (jsonObject.has("textFile2")) {
-                data.put("textFile2", jsonObject.get("textFile2").getAsString());
-            }
-            if (jsonObject.has("textFile3")) {
-                data.put("textFile3", jsonObject.get("textFile3").getAsString());
-            }
-            if (jsonObject.has("hd_texture")) {
-                data.put("hd_texture", jsonObject.get("hd_texture").getAsString());
-            }
-            if (jsonObject.has("selected_algorithm")) {
-                data.put("selected_algorithm", jsonObject.get("selected_algorithm").getAsString());
-            }
-            if (jsonObject.has("selected_texture_type")) {
-                data.put("selected_texture_type", jsonObject.get("selected_texture_type").getAsString());
-            }
-            if (jsonObject.has("token")) {
-                data.put("token", jsonObject.get("token").getAsString());
-            }
-
-
-            //Process data
-            String result = processImage(data);
-
-            //Return Result
+        // Parse the JSON request body
+        Gson gson = new Gson();
+        Map<String, String> data = new HashMap<>();
+        JsonObject jsonObject = gson.fromJson(request.getReader(), JsonObject.class);
+        if (jsonObject == null) {
             response.setContentType("application/json");
             response.setStatusCode(200);
-
-
-
-            JsonObject jsonObject1 = new JsonObject();
-            jsonObject1.addProperty("image", result); // Replace with valid base64 data
-            jsonObject1.addProperty("completion", 100);
-
-            BufferedWriter writer = response.getWriter();
-            writer.close();
-        }catch (RuntimeException ex) {
-            response.setContentType("application/json");
-            response.setStatusCode(500);
-
-            JsonObject jsonObject1 = new JsonObject();
-            jsonObject1.addProperty("completion", -1);
-            jsonObject1.addProperty("image", "");
-            String error = "ex.getMessage()\n";
-            for (int i = 0; i < ex.getStackTrace().length; i++) {
-                error = error+(ex.getStackTrace()[i].toString()+"\n");
-            }
-            jsonObject1.addProperty("error", error);
-
-            response.getWriter().write(new Gson().toJson(jsonObject1));
+            response.getWriter().write("jSonObject is null in ImageProcessor" + "\n");
+            return;
         }
+
+        if (jsonObject.has("image1")) {
+            data.put("image1", jsonObject.get("image1").getAsString());
+        }
+        if (jsonObject.has("model")) {
+            data.put("model", jsonObject.get("model").getAsString());
+        }
+        if (jsonObject.has("image3")) {
+            data.put("image3", jsonObject.get("image3").getAsString());
+        }
+        if (jsonObject.has("textFile1")) {
+            data.put("textFile1", jsonObject.get("textFile1").getAsString());
+        }
+        if (jsonObject.has("textFile2")) {
+            data.put("textFile2", jsonObject.get("textFile2").getAsString());
+        }
+        if (jsonObject.has("textFile3")) {
+            data.put("textFile3", jsonObject.get("textFile3").getAsString());
+        }
+        if (jsonObject.has("hd_texture")) {
+            data.put("hd_texture", jsonObject.get("hd_texture").getAsString());
+        }
+        if (jsonObject.has("selected_algorithm")) {
+            data.put("selected_algorithm", jsonObject.get("selected_algorithm").getAsString());
+        }
+        if (jsonObject.has("selected_texture_type")) {
+            data.put("selected_texture_type", jsonObject.get("selected_texture_type").getAsString());
+        }
+        if (jsonObject.has("token")) {
+            data.put("token", jsonObject.get("token").getAsString());
+        }
+
+
+        //Process data
+        String result = processImage(data);
+
+        //Return Result
+        response.setContentType("application/json");
+        response.setStatusCode(200);
+
+
+        JsonObject jsonObject1 = new JsonObject();
+        jsonObject1.addProperty("image", result); // Replace with valid base64 data
+        jsonObject1.addProperty("completion", 100);
+
+        BufferedWriter writer = response.getWriter();
+        writer.close();
+//        }catch (RuntimeException ex) {
+//            response.setContentType("application/json");
+//            response.setStatusCode(500);
+//
+//            JsonObject jsonObject1 = new JsonObject();
+//            jsonObject1.addProperty("completion", -1);
+//            jsonObject1.addProperty("image", "");
+//            String error = "ex.getMessage()\n";
+//            for (int i = 0; i < ex.getStackTrace().length; i++) {
+//                error = error+(ex.getStackTrace()[i].toString()+"\n");
+//            }
+//            jsonObject1.addProperty("error", error);
+//
+//            response.getWriter().write(new Gson().toJson(jsonObject1));
+//        }
     }
 
     private String processImage(Map<String, String> data) {
