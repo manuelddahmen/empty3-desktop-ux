c8f2e7679ad6f65ac8c78d169ff5c1b6a2b8ad0c
Manuel Daniel Dahmen <dathewolf@gmail.com>
2026-08-08 20:56:14 +0200

    Client web + serveur Java
    Signed-off-by: Manuel Daniel Dahmen <dathewolf@gmail.com>

--- client-web-game/build.bat
+++ client-web-game/build.bat
@@ -1,2 +1,2 @@
-docker build -t us-central1-docker.pkg.dev/padel-server-504118/game-repo/opad-proxy:0.2 -f Dockerfile-proxy .
-docker build -t us-central1-docker.pkg.dev/padel-server-504118/game-repo/opad-web:0.2 -f Dockerfile-web .
+docker build -t us-central1-docker.pkg.dev/padel-server-504118/game-repo/opad-proxy:0.4 -f Dockerfile-proxy .
+docker build -t us-central1-docker.pkg.dev/padel-server-504118/game-repo/opad-web:0.4 -f Dockerfile-web .
