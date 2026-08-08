a6e0db13357f6b0652201bac20ddc285e40f5032
Manuel Daniel Dahmen <dathewolf@gmail.com>
2026-08-08 19:37:32 +0200

    Client web + serveur Java
    Signed-off-by: Manuel Daniel Dahmen <dathewolf@gmail.com>

---
+++ client-web-game/build.bat
@@ -0,0 +1,2 @@
+docker build -t opad-proxy:0.1 -f Dockerfile-proxy .
+docker build -t opad-web:0.1 -f Dockerfile-web .
---
+++ client-web-game/run.bat
@@ -0,0 +1,2 @@
+docker run -p 8080:8080 opad-proxy:0.1
+docker run -p 80:80 opad-web:0.1
